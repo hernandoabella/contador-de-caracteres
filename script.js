c8c5fec029962e242bcf115e151d04626e34a9f5
Hernando Abella <hernandoabella@gmail.com>
2023-07-31 17:15:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let cadena = document.getElementById("cadena");
+let clearButton = document.getElementById("clear");
 
 let imprimir = (mensaje) => {
     let salida = document.getElementById("salida");
     salida.innerHTML = "<b>" + mensaje + "</b>";
 }
 
-cadena.addEventListener("keyup", e =>{
-    if(e){
-        e.preventDefault();
-        imprimir(document.getElementById('cadena').value.length);
-    }
+cadena.addEventListener("keyup", e => {
+    e.preventDefault();
+    imprimir(cadena.value.length);
 }, false);
 
+clearButton.addEventListener("click", () => {
+    cadena.value = ""; // Borra el texto del cuadro de texto
+    imprimir(cadena.value.length); // Actualiza el contador de caracteres (debería mostrar 0)
+});
